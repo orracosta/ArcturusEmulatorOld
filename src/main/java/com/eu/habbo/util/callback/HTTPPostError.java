d0d17249fb6daef3ca4746a481c51937a00c5c8d
Wesley12312 <heavenscraft@hotmail.com>
2016-08-04 14:29:46 +0200

    Updated HTTPPostError.java

--- src/main/java/com/eu/habbo/util/callback/HTTPPostError.java
+++ src/main/java/com/eu/habbo/util/callback/HTTPPostError.java
@@ -23,19 +23,12 @@ public class HTTPPostError implements Runnable
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         stackTrace.printStackTrace(pw);
-        sw.toString(); // stack trace as a string
-
         String url = "http://arcturus.wf/callback/error.php";
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-        //add reuqest header
         con.setRequestMethod("POST");
         con.setRequestProperty("User-Agent", "arcturus");
-
         String urlParameters = "errors=1&version=" + Emulator.version + "&stacktrace=" + sw.toString() + "&users=" + Emulator.getGameEnvironment().getHabboManager().getOnlineCount() + "&rooms=" + Emulator.getGameEnvironment().getRoomManager().getActiveRooms().size() + "&username=" + Emulator.getConfig().getValue("username");
-
-        // Send post request
         con.setDoOutput(true);
         DataOutputStream wr = new DataOutputStream(con.getOutputStream());
         wr.writeBytes(urlParameters);
