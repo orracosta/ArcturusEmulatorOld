daf2df46e48826f6817c40383409bd8a4c418076
Wesley12312 <heavenscraft@hotmail.com>
2016-08-04 14:27:16 +0200

    Updated 1.0.4 to 1.0.5 update sql.

--- sqlupdates/1_0_4_TO_1_0_5.sql
+++ sqlupdates/1_0_4_TO_1_0_5.sql
@@ -2,7 +2,9 @@ INSERT INTO emulator_settings (`key`, `value`) VALUES
         ('camera.enabled', '1'),
         ('camera.price.credits', '100'),
         ('camera.price.points', '100'),
-        ('camera.price.points.publish', '1000');
+        ('camera.price.points.publish', '1000'),
+        ('camera.item_id', '23425'),
+        ('camera.extradata', '{"t":%timestamp%, "u":"%id%", "s":%room_id%, "w":"%url%"}');
 
 INSERT INTO `emulator_texts` (`key`, `value`) VALUES
         ('commands.error.cmd_kick.unkickable', '%username% is unkickable!');
