3e6b1c41b53e4002162b5192c33919ad7ab0e171
Wesley12312 <heavenscraft@hotmail.com>
2016-08-20 12:26:35 +0200

    Fixed bug in buying photos not directly being added to the inventory.

--- src/main/java/com/eu/habbo/messages/incoming/camera/CameraPurchaseEvent.java
+++ src/main/java/com/eu/habbo/messages/incoming/camera/CameraPurchaseEvent.java
@@ -5,6 +5,7 @@ import com.eu.habbo.habbohotel.users.HabboItem;
 import com.eu.habbo.messages.incoming.MessageHandler;
 import com.eu.habbo.messages.outgoing.camera.CameraPurchaseSuccesfullComposer;
 import com.eu.habbo.messages.outgoing.inventory.AddHabboItemComposer;
+import com.eu.habbo.messages.outgoing.inventory.InventoryRefreshComposer;
 
 public class CameraPurchaseEvent extends MessageHandler
 {
@@ -24,8 +25,9 @@ public class CameraPurchaseEvent extends MessageHandler
                         photoItem.setExtradata(photoItem.getExtradata().replace("%id%", photoItem.getId() + ""));
                         photoItem.needsUpdate(true);
                         this.client.getHabbo().getHabboInventory().getItemsComponent().addItem(photoItem);
-                        this.client.sendResponse(new AddHabboItemComposer(photoItem));
+                        this.client.sendResponse(new InventoryRefreshComposer());
                         this.client.sendResponse(new CameraPurchaseSuccesfullComposer());
+                        this.client.sendResponse(new AddHabboItemComposer(photoItem));
                         this.client.getHabbo().giveCredits(-Emulator.getConfig().getInt("camera.price.credits"));
                         this.client.getHabbo().givePixels(-Emulator.getConfig().getInt("camera.price.points"));
                     }
