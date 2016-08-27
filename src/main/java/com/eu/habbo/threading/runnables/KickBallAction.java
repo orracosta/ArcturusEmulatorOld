b8df666c24255001955174cdb6395fff49f45e34
Beny <ben@fish.cat>
2016-08-27 17:25:22 +0000

    KickBallAction changed to generic Pushable

--- src/main/java/com/eu/habbo/threading/runnables/KickBallAction.java
+++ src/main/java/com/eu/habbo/threading/runnables/KickBallAction.java
@@ -1,27 +1,25 @@
 package com.eu.habbo.threading.runnables;
 
 import com.eu.habbo.Emulator;
-import com.eu.habbo.habbohotel.items.interactions.games.football.InteractionFootball;
+import com.eu.habbo.habbohotel.items.interactions.InteractionPushable;
 import com.eu.habbo.habbohotel.rooms.Room;
 import com.eu.habbo.util.pathfinding.PathFinder;
 import com.eu.habbo.util.pathfinding.Tile;
 import com.eu.habbo.habbohotel.rooms.RoomUnit;
 import com.eu.habbo.habbohotel.rooms.RoomUserRotation;
-import com.eu.habbo.habbohotel.users.HabboItem;
 import com.eu.habbo.messages.outgoing.rooms.items.FloorItemOnRollerComposer;
-import com.eu.habbo.messages.outgoing.rooms.items.ItemStateComposer;
 
 public class KickBallAction implements Runnable
 {
-    private final InteractionFootball ball;
-    private final Room room;
-    private final RoomUnit kicker;
-    private RoomUserRotation currentDirection;
-    private int totalSteps;
-    private int currentStep;
-    public Boolean dead = false;
+    private final InteractionPushable ball; //The item which is moving
+    private final Room room; //The room that the item belongs to
+    private final RoomUnit kicker; //The Habbo which initiated the move of the item
+    private RoomUserRotation currentDirection; //The current direction the item is moving in
+    private final int totalSteps; //The total number of steps in the move sequence
+    private int currentStep; //The current step of the move sequence
+    public boolean dead = false; //When true the run() function will not execute. Used when another user kicks the ball whilst it is arleady moving.
 
-    public KickBallAction(InteractionFootball ball, Room room, RoomUnit kicker, RoomUserRotation direction, int steps)
+    public KickBallAction(InteractionPushable ball, Room room, RoomUnit kicker, RoomUserRotation direction, int steps)
     {
         this.ball = ball;
         this.room = room;
@@ -30,143 +28,58 @@ public class KickBallAction implements Runnable
         this.totalSteps = steps;
         this.currentStep = 0;
     }
-    
-    private Boolean validTile(Tile tile)
-    {
-        return validTile(tile.x, tile.y);
-    }
-    
-    private Boolean validTile(int x, int y)
-    {
-        HabboItem topItem = room.getTopItemAt(x, y, this.ball);
-        return room.tileWalkable(new Tile(x, y)) || !(topItem == null || topItem.getBaseItem().allowStack());
-    }
 
     @Override
     public void run()
     {
-        if(dead)
+        if(dead || !this.room.isLoaded())
             return;
         
         if(currentStep < totalSteps)
         {            
-            Tile next = PathFinder.getSquareInFront(this.ball.getX(), this.ball.getY(), this.currentDirection.getValue());
-            
-            HabboItem topItem = room.getTopItemAt(this.ball.getX(), this.ball.getY(), this.ball);
-            
-            if (!validTile(next))
+            Tile next = PathFinder.getSquareInFront(this.ball.getX(), this.ball.getY(), this.currentDirection.getValue());            
+            if (!this.ball.validMove(this.room, new Tile(this.ball.getX(), this.ball.getY()), next))
             {
-                this.currentDirection = changeDirection(this.currentDirection);
+                RoomUserRotation oldDirection = this.currentDirection;
+                this.currentDirection = this.ball.getBounceDirection(this.room, this.currentDirection);
+                if(this.currentDirection != oldDirection)
+                {
+                    this.ball.onBounce(room, oldDirection, this.currentDirection, this.kicker);
+                }
+                else
+                {
+                    currentStep = totalSteps; //End the move sequence, the ball can't bounce anywhere
+                }
                 run();
             }
             else
             {
                 //Move the ball & run again
-                currentStep++; 
+                currentStep++;
                 
-                float t = 2500; //Time for ball to finish it's cycle
-                float delay = (totalSteps == 1) ? 500 : 100*((t=t/t-1)*t*t*t*t + 1) + (currentStep * 100); //Algorithm to work out the delay till next run
+                int delay = this.ball.getNextRollDelay(this.currentStep, this.totalSteps); //Algorithm to work out the delay till next run
                 
-                this.ball.setExtradata(delay <= 200 ? "8" : (delay <= 250 ? "7" : (delay <= 300 ? "6" : (delay <= 350 ? "5" : (delay <= 400 ? "4" : (delay <= 450 ? "3" : (delay <= 500 ? "2" : "1")))))));
-                this.room.sendComposer(new ItemStateComposer(this.ball).compose());
+                if(this.ball.canStillMove(room, new Tile(this.ball.getX(), this.ball.getY()), next, this.currentDirection, this.kicker, delay, this.currentStep, this.totalSteps))
+                {
+                    this.ball.onMove(room, new Tile(this.ball.getX(), this.ball.getY()), next, this.currentDirection, this.kicker, delay, this.currentStep, this.totalSteps);
                 
-                next.Z = room.getStackHeight(next.X, next.Y, false);
-                this.room.sendComposer(new FloorItemOnRollerComposer(this.ball, null, next, this.room).compose());                
-                
-                Emulator.getThreading().run(this, (long)delay);
+                    next.Z = room.getStackHeight(next.X, next.Y, false);
+                    this.room.sendComposer(new FloorItemOnRollerComposer(this.ball, null, next, this.room).compose());                
+
+                    Emulator.getThreading().run(this, (long)delay);
+                }
+                else
+                {
+                    currentStep = totalSteps; //End the move sequence, the ball can't bounce anywhere
+                    run();
+                }
             }
         }
         else
         {
-            //We're done with the football. Stop it from rolling & end the thread.
-            if(!this.ball.getExtradata().equals("0"))
-            {
-                this.ball.setExtradata("0");
-                this.room.sendComposer(new ItemStateComposer(this.ball).compose());
-                dead = true;
-            }
-        }
-    }
-
-    private RoomUserRotation changeDirection(RoomUserRotation direction)
-    {
-        switch(direction)
-        {
-            default:
-            case NORTH:
-                return RoomUserRotation.SOUTH;
-            
-            case NORTH_EAST:
-                if(validTile(PathFinder.getSquareInFront(this.ball.getX(), this.ball.getY(), RoomUserRotation.NORTH_WEST.getValue())))
-                    return RoomUserRotation.NORTH_WEST;
-                else if(validTile(PathFinder.getSquareInFront(this.ball.getX(), this.ball.getY(), RoomUserRotation.SOUTH_EAST.getValue())))
-                    return RoomUserRotation.SOUTH_EAST;
-                else
-                    return RoomUserRotation.SOUTH_WEST;
-                
-            case EAST:
-                return RoomUserRotation.WEST;
-                
-            case SOUTH_EAST:
-                if(validTile(PathFinder.getSquareInFront(this.ball.getX(), this.ball.getY(), RoomUserRotation.SOUTH_WEST.getValue())))
-                    return RoomUserRotation.SOUTH_WEST;
-                else if(validTile(PathFinder.getSquareInFront(this.ball.getX(), this.ball.getY(), RoomUserRotation.NORTH_EAST.getValue())))
-                    return RoomUserRotation.NORTH_EAST;
-                else
-                    return RoomUserRotation.NORTH_WEST;
-                
-            case SOUTH:
-                return RoomUserRotation.NORTH;
-                
-            case SOUTH_WEST:
-                if(validTile(PathFinder.getSquareInFront(this.ball.getX(), this.ball.getY(), RoomUserRotation.SOUTH_EAST.getValue())))
-                    return RoomUserRotation.SOUTH_EAST;
-                else if(validTile(PathFinder.getSquareInFront(this.ball.getX(), this.ball.getY(), RoomUserRotation.SOUTH_EAST.getValue())))
-                    return RoomUserRotation.NORTH_WEST;
-                else
-                    return RoomUserRotation.NORTH_EAST;
-                
-            case WEST:
-                return RoomUserRotation.EAST;
-                
-            case NORTH_WEST:
-                if(validTile(PathFinder.getSquareInFront(this.ball.getX(), this.ball.getY(), RoomUserRotation.NORTH_EAST.getValue())))
-                    return RoomUserRotation.NORTH_EAST;
-                else if(validTile(PathFinder.getSquareInFront(this.ball.getX(), this.ball.getY(), RoomUserRotation.SOUTH_WEST.getValue())))
-                    return RoomUserRotation.SOUTH_WEST;
-                else
-                    return RoomUserRotation.SOUTH_EAST;
-        }
-    }
-    
-    public static RoomUserRotation oppositeDirection(RoomUserRotation direction)
-    {
-        switch(direction)
-        {
-            default:
-            case NORTH:
-                return RoomUserRotation.SOUTH;
-            
-            case NORTH_EAST:
-                return RoomUserRotation.SOUTH_WEST;
-                
-            case EAST:
-                return RoomUserRotation.WEST;
-                
-            case SOUTH_EAST:
-                    return RoomUserRotation.NORTH_WEST;
-                
-            case SOUTH:
-                return RoomUserRotation.NORTH;
-                
-            case SOUTH_WEST:
-                    return RoomUserRotation.NORTH_EAST;
-                
-            case WEST:
-                return RoomUserRotation.EAST;
-                
-            case NORTH_WEST:
-                    return RoomUserRotation.SOUTH_EAST;
+            //We're done with the move sequence. Stop it the sequence & end the thread.
+            this.ball.onStop(room, this.kicker, this.currentStep, this.totalSteps);
+            dead = true;
         }
     }
 }
