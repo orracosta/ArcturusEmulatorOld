599124ed47abb131813035653870ae21dea11024
Wesley12312 <heavenscraft@hotmail.com>
2017-02-11 20:00:23 +0100

    Reverted onClick in InteractionGate.java

--- src/main/java/com/eu/habbo/habbohotel/items/interactions/InteractionGate.java
+++ src/main/java/com/eu/habbo/habbohotel/items/interactions/InteractionGate.java
@@ -46,10 +46,7 @@ public class InteractionGate extends HabboItem
     {
         super.onClick(client, room, objects);
 
-        if (client == null)
-            return;
-        
-        if (!room.hasRights(client.getHabbo()))
+        if (client != null && !room.hasRights(client.getHabbo()))
             return;
 
         if(!room.getHabbosAt(this.getX(), this.getY()).isEmpty())
