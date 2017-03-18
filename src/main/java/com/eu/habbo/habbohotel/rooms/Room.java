7544f61c28b9adfbac2213b34e760f30c135adc6
Wesley12312 <heavenscraft@hotmail.com>
2017-03-18 22:23:32 +0100

    Fixed nullpointer when kicking people.

--- src/main/java/com/eu/habbo/habbohotel/rooms/Room.java
+++ src/main/java/com/eu/habbo/habbohotel/rooms/Room.java
@@ -2818,7 +2818,7 @@ public class Room implements Comparable<Room>, ISerialize, Runnable
         habbo.getRoomUnit().isKicked = true;
         habbo.getRoomUnit().setGoalLocation(this.layout.getDoorTile());
 
-        if (habbo.getRoomUnit().getPathFinder().getPath().poll() == null)
+        if (habbo.getRoomUnit().getPathFinder().getPath() == null)
         {
             habbo.getRoomUnit().setCanWalk(true);
             Emulator.getGameEnvironment().getRoomManager().leaveRoom(habbo, this);
