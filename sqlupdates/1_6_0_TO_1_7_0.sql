74fc352d273d9575c4e9fcd6938aea60f614e09f
Wesley12312 <heavenscraft@hotmail.com>
2017-05-03 20:19:20 +0200

    Fixed bug in CloseDiceEvent packet not checking for dice.

--- sqlupdates/1_6_0_TO_1_7_0.sql
+++ sqlupdates/1_6_0_TO_1_7_0.sql
@@ -1,5 +1,5 @@
 #DATABASE UPDATE: 1.6.0 -> 1.7.0
 
-
+INSERT INTO `emulator_texts` (`key`, `value`) VALUES ('scripter.warning.packet.closedice', '%username% tried to change furniture state on non dice using the close dice packet on item %id% %itemname%');
 
 #END DATABASE UPDATE: 1.6.0 -> 1.7.0
--- src/main/java/com/eu/habbo/messages/incoming/rooms/items/CloseDiceEvent.java
+++ src/main/java/com/eu/habbo/messages/incoming/rooms/items/CloseDiceEvent.java
@@ -1,6 +1,7 @@
 package com.eu.habbo.messages.incoming.rooms.items;
 
 import com.eu.habbo.Emulator;
+import com.eu.habbo.habbohotel.items.interactions.InteractionDice;
 import com.eu.habbo.habbohotel.rooms.Room;
 import com.eu.habbo.habbohotel.users.HabboItem;
 import com.eu.habbo.messages.incoming.MessageHandler;
@@ -23,15 +24,22 @@ public class CloseDiceEvent extends MessageHandler
 
         if(item != null)
         {
-            if(PathFinder.tilesAdjecent(item.getX(), item.getY(), this.client.getHabbo().getRoomUnit().getX(), this.client.getHabbo().getRoomUnit().getY()))
+            if (item instanceof InteractionDice)
             {
-                if(!item.getExtradata().equals("-1"))
+                if (PathFinder.tilesAdjecent(item.getX(), item.getY(), this.client.getHabbo().getRoomUnit().getX(), this.client.getHabbo().getRoomUnit().getY()))
                 {
-                    item.setExtradata("0");
-                    item.needsUpdate(true);
-                    Emulator.getThreading().run(item);
-                    room.updateItem(item);
+                    if (!item.getExtradata().equals("-1"))
+                    {
+                        item.setExtradata("0");
+                        item.needsUpdate(true);
+                        Emulator.getThreading().run(item);
+                        room.updateItem(item);
+                    }
                 }
+            }
+            else
+            {
+                Emulator.getGameEnvironment().getModToolManager().quickTicket(this.client.getHabbo(), "Scripter", Emulator.getTexts().getValue("scripter.warning.packet.closedice").replace("%username%", this.client.getHabbo().getHabboInfo().getUsername()).replace("%id%", item.getId() + "").replace("%itemname%", item.getBaseItem().getName()));
             }
         }
     }
