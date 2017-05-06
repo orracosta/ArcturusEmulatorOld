7e122add538953dbbd4d0d535bd34c03c3f1c0c6
Wesley12312 <heavenscraft@hotmail.com>
2017-05-06 20:23:07 +0200

    Added check to cmd_lay to make sure tiles are walkable.

--- src/main/java/com/eu/habbo/habbohotel/commands/LayCommand.java
+++ src/main/java/com/eu/habbo/habbohotel/commands/LayCommand.java
@@ -2,8 +2,11 @@ package com.eu.habbo.habbohotel.commands;
 
 import com.eu.habbo.Emulator;
 import com.eu.habbo.habbohotel.gameclients.GameClient;
+import com.eu.habbo.habbohotel.rooms.RoomLayout;
+import com.eu.habbo.habbohotel.rooms.RoomTile;
 import com.eu.habbo.habbohotel.rooms.RoomUserRotation;
 import com.eu.habbo.messages.outgoing.rooms.users.RoomUserStatusComposer;
+import com.eu.habbo.util.pathfinding.PathFinder;
 
 public class LayCommand extends Command
 {
@@ -19,6 +22,16 @@ public class LayCommand extends Command
         gameClient.getHabbo().getHabboInfo().getCurrentRoom().updateHabbo(gameClient.getHabbo());
         gameClient.getHabbo().getRoomUnit().cmdSit = true;
         gameClient.getHabbo().getRoomUnit().setBodyRotation(RoomUserRotation.values()[gameClient.getHabbo().getRoomUnit().getBodyRotation().getValue() - gameClient.getHabbo().getRoomUnit().getBodyRotation().getValue() % 2]);
+
+        RoomTile tile = gameClient.getHabbo().getRoomUnit().getCurrentLocation();
+        for (int i = 0; i < 3; i++)
+        {
+            if (!gameClient.getHabbo().getHabboInfo().getCurrentRoom().getLayout().getTileInFront(tile, gameClient.getHabbo().getRoomUnit().getBodyRotation().getValue(), i).isWalkable())
+            {
+                return false;
+            }
+        }
+        
         gameClient.getHabbo().getRoomUnit().getStatus().put("lay", 0.5 + "");
         gameClient.getHabbo().getHabboInfo().getCurrentRoom().sendComposer(new RoomUserStatusComposer(gameClient.getHabbo().getRoomUnit()).compose());
         return true;
