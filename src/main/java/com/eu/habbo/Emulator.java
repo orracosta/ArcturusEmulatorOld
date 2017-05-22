b6ed1aeee6b1a5aaa5a7fad161d57c612f84a0ba
Wesley12312 <heavenscraft@hotmail.com>
2017-05-22 23:06:38 +0200

    Start 1.8.0

--- src/main/java/com/eu/habbo/Emulator.java
+++ src/main/java/com/eu/habbo/Emulator.java
@@ -37,7 +37,7 @@ public final class Emulator
     /**
      * Minor version of the emulator.
      */
-    public final static int MINOR = 7;
+    public final static int MINOR = 8;
 
     /**
      * Stable build version of the emulator.
