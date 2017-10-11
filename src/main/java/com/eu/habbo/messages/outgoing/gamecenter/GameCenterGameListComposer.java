bb18273c55164d0a981021a98039530814ca9c9c
Wesley12312 <heavenscraft@hotmail.com>
2017-10-11 03:13:21 +0200

    Updated config.ini

--- src/main/java/com/eu/habbo/messages/outgoing/gamecenter/GameCenterGameListComposer.java
+++ src/main/java/com/eu/habbo/messages/outgoing/gamecenter/GameCenterGameListComposer.java
@@ -16,14 +16,14 @@ public class GameCenterGameListComposer extends MessageComposer
         this.response.appendString("snowwar");
         this.response.appendString("93d4f3");
         this.response.appendString("");
-        this.response.appendString("http://localhost/game/c_images/gamecenter_snowwar/");
+        this.response.appendString("c_images/gamecenter_snowwar/");
         this.response.appendString("");
 
         this.response.appendInt(3);
         this.response.appendString("basejump");
         this.response.appendString("68bbd2"); //Background Color
         this.response.appendString(""); //Text color
-        this.response.appendString("http://localhost/game/c_images/gamecenter_basejump/");
+        this.response.appendString("c_images/gamecenter_basejump/");
         this.response.appendString("");
 
         this.response.appendInt(4);
