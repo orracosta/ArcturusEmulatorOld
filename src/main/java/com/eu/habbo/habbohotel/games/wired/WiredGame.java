4bd8d746a1ab18b7d7f16e31e2ff95f525eb981c
Rafael Costa <orracosta@icloud.com>
2017-10-25 21:37:09 -0200

    Little fix wird game

--- src/main/java/com/eu/habbo/habbohotel/games/wired/WiredGame.java
+++ src/main/java/com/eu/habbo/habbohotel/games/wired/WiredGame.java
@@ -181,6 +181,9 @@ public class WiredGame extends Game
     @Override
     public boolean addHabbo(Habbo habbo, GameTeamColors teamColor)
     {
+        this.removeHabbo(habbo);
+        this.room.giveEffect(habbo, 0);
+
         this.room.giveEffect(habbo, FreezeGame.effectId + teamColor.type);
         return super.addHabbo(habbo, teamColor);
     }
