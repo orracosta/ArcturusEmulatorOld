1269017ed6a3cc2d334ae389e2d184d9322fda4b
mrcrassi <flavio.tonon@icloud.com>
2017-10-28 21:18:04 -0200

    Added code to WiredChangeFurniDirection
    Added code to WiredChangeFurniDirection interaction type

--- src/main/java/com/eu/habbo/habbohotel/items/interactions/wired/effects/WiredEffectChangeFurniDirection.java
+++ src/main/java/com/eu/habbo/habbohotel/items/interactions/wired/effects/WiredEffectChangeFurniDirection.java
@@ -5,23 +5,30 @@ import com.eu.habbo.habbohotel.gameclients.GameClient;
 import com.eu.habbo.habbohotel.items.Item;
 import com.eu.habbo.habbohotel.items.interactions.InteractionWiredEffect;
 import com.eu.habbo.habbohotel.rooms.Room;
+import com.eu.habbo.habbohotel.rooms.RoomTile;
 import com.eu.habbo.habbohotel.rooms.RoomUnit;
 import com.eu.habbo.habbohotel.users.HabboItem;
 import com.eu.habbo.habbohotel.wired.WiredEffectType;
 import com.eu.habbo.habbohotel.wired.WiredHandler;
 import com.eu.habbo.messages.ClientMessage;
 import com.eu.habbo.messages.ServerMessage;
-import gnu.trove.map.hash.THashMap;
+import com.eu.habbo.messages.outgoing.rooms.items.FloorItemOnRollerComposer;
+import gnu.trove.set.hash.THashSet;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WiredEffectChangeFurniDirection extends InteractionWiredEffect
 {
     public static final WiredEffectType type = WiredEffectType.MOVE_DIRECTION;
-
-    private final THashMap<HabboItem, Integer> items = new THashMap<HabboItem, Integer>();
+    private final List<HabboItem> items = new ArrayList<>();
+    private int direction;
+    private int spacing = 1;
+    private Map<Integer, Integer> indexOffset = new LinkedHashMap<>();
 
     public WiredEffectChangeFurniDirection(ResultSet set, Item baseItem) throws SQLException
     {
@@ -34,28 +41,36 @@ public class WiredEffectChangeFurniDirection extends InteractionWiredEffect
     }
 
     @Override
-    public boolean execute(RoomUnit roomUnit, Room room, Object[] stuff)
+    public boolean saveData(ClientMessage packet, GameClient gameClient)
     {
-        return false;
-    }
+        Room room = Emulator.getGameEnvironment().getRoomManager().getRoom(this.getRoomId());
 
-    @Override
-    public String getWiredData()
-    {
-        return "";
-    }
+        if(room == null)
+            return false;
 
-    @Override
-    public void loadWiredData(ResultSet set, Room room) throws SQLException
-    {
-
-    }
-
-    @Override
-    public void onPickUp()
-    {
-        this.setDelay(0);
         this.items.clear();
+        this.indexOffset.clear();
+
+        packet.readInt();
+
+        this.direction = packet.readInt();
+        this.spacing = packet.readInt();
+        packet.readString();
+
+        int count = packet.readInt();
+        for (int i = 0; i < count; i++)
+        {
+            HabboItem item = room.getHabboItem(packet.readInt());
+            if(item != null)
+            {
+                item.setRotation(this.direction);
+            }
+            this.items.add(item);
+        }
+
+        this.setDelay(packet.readInt());
+
+        return true;
     }
 
     @Override
@@ -65,28 +80,236 @@ public class WiredEffectChangeFurniDirection extends InteractionWiredEffect
     }
 
     @Override
-    public void serializeWiredData(ServerMessage message, Room room)
+    public boolean execute(RoomUnit roomUnit, Room room, Object[] stuff)
     {
-        message.appendBoolean(false);
-        message.appendInt(WiredHandler.MAXIMUM_FURNI_SELECTION);
-        message.appendInt(this.items.size());
-        for (Map.Entry<HabboItem, Integer> item : this.items.entrySet())
+        List<HabboItem> items = new ArrayList<HabboItem>();
+
+        synchronized (this.items)
         {
-            message.appendInt(item.getKey().getId());
+            for (HabboItem item : this.items)
+            {
+                if (Emulator.getGameEnvironment().getRoomManager().getRoom(this.getRoomId()).getHabboItem(item.getId()) == null)
+                    items.add(item);
+            }
+
+            for (HabboItem item : items)
+            {
+                this.items.remove(item);
+            }
+
+
+            if (this.items.isEmpty())
+                return false;
+
+            if (stuff != null && stuff.length > 0)
+            {
+                for (HabboItem item : this.items)
+                {
+                    //if (object instanceof HabboItem)
+                    //{
+                        HabboItem targetItem = item;
+
+                        if (targetItem != null)
+                        {
+                            int indexOffset = 0;
+                            if (!this.indexOffset.containsKey(targetItem.getId()))
+                            {
+                                this.indexOffset.put(targetItem.getId(), indexOffset);
+                            }
+                            else
+                            {
+                                indexOffset = this.indexOffset.get(targetItem.getId());
+                            }
+
+                            RoomTile objectTile = room.getLayout().getTile(((HabboItem) targetItem).getX(), ((HabboItem) targetItem).getY());
+
+                            if (objectTile != null)
+                            {
+                                THashSet<RoomTile> refreshTiles = room.getLayout().getTilesAt(room.getLayout().getTile(((HabboItem) targetItem).getX(), ((HabboItem) targetItem).getY()), ((HabboItem) targetItem).getBaseItem().getWidth(), ((HabboItem) targetItem).getBaseItem().getLength(), ((HabboItem) targetItem).getRotation());
+
+                                RoomTile tile = room.getLayout().getTileInFront(objectTile, targetItem.getRotation(), indexOffset);
+                                RoomTile tilenext = room.getLayout().getTileInFront(objectTile, targetItem.getRotation(), indexOffset+1);
+                                if (tilenext == null || !tile.allowStack())
+                                {
+                                    switch (this.spacing)
+                                    {
+                                        case 1:
+                                            targetItem.setRotation(targetItem.getRotation()+1);
+                                            break;
+                                        case 2:
+                                            targetItem.setRotation(targetItem.getRotation()+2);
+                                            break;
+                                        case 3:
+                                            targetItem.setRotation(targetItem.getRotation()-1);
+                                            break;
+                                        case 4:
+                                            targetItem.setRotation(targetItem.getRotation()-2);
+                                            break;
+                                        case 5:
+                                            if(targetItem.getRotation() == 0) {
+                                                targetItem.setRotation(4);
+                                                continue;
+                                            }
+                                            if(targetItem.getRotation() == 1) {
+                                                targetItem.setRotation(5);
+                                                continue;
+                                            }
+                                            if(targetItem.getRotation() == 2) {
+                                                targetItem.setRotation(6);
+                                                continue;
+                                            }
+                                            if(targetItem.getRotation() == 3) {
+                                                targetItem.setRotation(7);
+                                                continue;
+                                            }
+                                            if(targetItem.getRotation() == 4) {
+                                                targetItem.setRotation(0);
+                                                continue;
+                                            }
+                                            if(targetItem.getRotation() == 5) {
+                                                targetItem.setRotation(1);
+                                                continue;
+                                            }
+                                            if(targetItem.getRotation() == 6) {
+                                                targetItem.setRotation(2);
+                                                continue;
+                                            }
+                                            if(targetItem.getRotation() == 7) {
+                                                targetItem.setRotation(3);
+                                                continue;
+                                            }
+                                            break;
+                                        case 6:
+                                            targetItem.setRotation(Emulator.getRandom().nextInt(7));
+                                            break;
+                                    }
+
+                                    if(targetItem.getRotation() > 7) {
+                                        targetItem.setRotation(0);
+                                    }
+
+                                    if(targetItem.getRotation() < 0) {
+                                        targetItem.setRotation(7);
+                                    }
+
+                                }
+                                else
+                                {
+                                    room.sendComposer(new FloorItemOnRollerComposer((HabboItem) targetItem, null, tile, tile.getStackHeight() - ((HabboItem) targetItem).getZ(), room).compose());
+                                    refreshTiles.addAll(room.getLayout().getTilesAt(room.getLayout().getTile(((HabboItem) targetItem).getX(), ((HabboItem) targetItem).getY()), ((HabboItem) targetItem).getBaseItem().getWidth(), ((HabboItem) targetItem).getBaseItem().getLength(), ((HabboItem) targetItem).getRotation()));
+                                    room.updateTiles(refreshTiles);
+                                    this.indexOffset.put(targetItem.getId(), indexOffset);
+                                }
+                            }
+                        }
+                    //}
+                }
+            }
         }
-        message.appendInt(this.getBaseItem().getSpriteId());
-        message.appendInt(this.getId());
-        message.appendString("");
-        message.appendInt(0);
-        message.appendInt(0);
-        message.appendInt(this.getType().code);
-        message.appendInt(this.getDelay());
-        message.appendInt(0);
+        return true;
     }
 
     @Override
-    public boolean saveData(ClientMessage packet, GameClient gameClient)
+    public String getWiredData()
     {
-        return false;
+        THashSet<HabboItem> items = new THashSet<HabboItem>();
+
+        for(HabboItem item : this.items)
+        {
+            if(item.getRoomId() != this.getRoomId() || Emulator.getGameEnvironment().getRoomManager().getRoom(this.getRoomId()).getHabboItem(item.getId()) == null)
+                items.add(item);
+        }
+
+        for(HabboItem item : items)
+        {
+            this.items.remove(item);
+        }
+
+        String data = this.direction + "\t" + this.spacing + "\t" + this.getDelay() + "\t";
+
+        for(HabboItem item : this.items)
+        {
+            data += item.getId() + "\r";
+        }
+
+        return data;
+    }
+
+    @Override
+    public void serializeWiredData(ServerMessage message, Room room)
+    {
+        THashSet<HabboItem> items = new THashSet<HabboItem>();
+
+        synchronized (this.items)
+        {
+            for (HabboItem item : this.items)
+            {
+                if (item.getRoomId() != this.getRoomId() || Emulator.getGameEnvironment().getRoomManager().getRoom(this.getRoomId()).getHabboItem(item.getId()) == null)
+                    items.add(item);
+            }
+
+            for (HabboItem item : items)
+            {
+                this.items.remove(item);
+            }
+
+            message.appendBoolean(false);
+            message.appendInt(WiredHandler.MAXIMUM_FURNI_SELECTION);
+            message.appendInt(this.items.size());
+            for (HabboItem item : this.items)
+                message.appendInt(item.getId());
+            message.appendInt(this.getBaseItem().getSpriteId());
+            message.appendInt(this.getId());
+            message.appendString("");
+            message.appendInt(2);
+            message.appendInt(this.direction);
+            message.appendInt(this.spacing);
+            message.appendInt(0);
+            message.appendInt(this.getType().code);
+            message.appendInt(this.getDelay());
+            message.appendInt(0);
+        }
+    }
+
+    @Override
+    public void loadWiredData(ResultSet set, Room room) throws SQLException
+    {
+        synchronized (this.items)
+        {
+            this.items.clear();
+
+            String[] data = set.getString("wired_data").split("\t");
+
+            if (data.length == 4)
+            {
+                try
+                {
+                    this.direction = Integer.valueOf(data[0]);
+                    this.spacing = Integer.valueOf(data[1]);
+                    this.setDelay(Integer.valueOf(data[2]));
+                }
+                catch (Exception e)
+                {
+                }
+
+                for (String s : data[3].split("\r"))
+                {
+                    HabboItem item = room.getHabboItem(Integer.valueOf(s));
+
+                    if (item != null)
+                        this.items.add(item);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onPickUp()
+    {
+        this.setDelay(0);
+        this.items.clear();
+        this.direction = 0;
+        this.spacing = 0;
+        this.indexOffset.clear();
     }
 }
