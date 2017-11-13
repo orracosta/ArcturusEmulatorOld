8d219dfef6fd24a1a47e8b71ca2258ea73d1cce5
Rafael Costa <orracosta@icloud.com>
2017-11-13 10:04:40 -0200

    Fix findpath to wireds

--- src/main/java/com/eu/habbo/habbohotel/rooms/RoomLayout.java
+++ src/main/java/com/eu/habbo/habbohotel/rooms/RoomLayout.java
@@ -298,7 +298,7 @@ public class RoomLayout
                     if ((!ALLOW_FALLING && height < -MAXIMUM_STEP_HEIGHT) || (height > MAXIMUM_STEP_HEIGHT && !room.canLayAt(currentAdj.x, currentAdj.y)))
                         continue;
 
-                    if (!this.room.isAllowWalkthrough() && room.hasHabbosAt(currentAdj.x, currentAdj.y)) continue;
+                    if (notIsFurni && (!this.room.isAllowWalkthrough() && room.hasHabbosAt(currentAdj.x, currentAdj.y))) continue;
 
                     //if (room.hasPetsAt(currentAdj.x, currentAdj.y)) continue;
 
