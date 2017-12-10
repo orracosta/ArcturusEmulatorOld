65c45b848b373122c4d0f76038302161bebf2d03
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-12-10 12:03:36 -0200

    Delete sql-updates.sql

--- sql-updates.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `achievements`
-CHANGE COLUMN `pixels` `reward_amount`  int(11) NOT NULL DEFAULT 100 AFTER `level`,
-ADD COLUMN `reward_currency`  int(11) NOT NULL DEFAULT 0 AFTER `level`;
-
